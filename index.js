10b33c84dda540f11bb1f200aed166fcfa4087ce
nateThickett <92690338+nateThickett@users.noreply.github.com>
2022-05-09 18:52:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+var button = document.getElementById("create-twit-button");
+var modalBackdrop = document.getElementById("modal-backdrop");
+var modal = document.getElementById("create-twit-modal");
+var modalCancelButton  = document.getElementsByClassName("modal-cancel-button")[0];
+var modalCloseButton = document.getElementsByClassName("modal-close-button")[0];
+var modalAcceptButton = document.getElementsByClassName("modal-accept-button")[0];
+var twitText = document.getElementById("twit-text-input");
+var twitAuthor = document.getElementById("twit-attribution-input");
+
+
+
+button.addEventListener('click', modalToggle);
+modalCancelButton.addEventListener('click', modalToggle);
+modalCloseButton.addEventListener('click', modalToggle);
+modalAcceptButton.addEventListener('click', addTwit);
+
+function modalToggle(event) {
+  if (modal.classList.contains("hidden")) {
+    twitText.value = "";
+    twitAuthor.value = "";
+    modal.classList.remove('hidden');
+    modalBackdrop.classList.remove('hidden');
+  }
+  else {
+    modal.classList.add('hidden');
+    modalBackdrop.classList.add('hidden');
+  }
+}
+
 function addTwit(event) {
   if ((twitText.value == "") || (twitAuthor.value == "")) {
     alert("You have not entered a value for either text or author")
@@ -45,4 +74,20 @@ document.getElementsByClassName("twit-container")[0].insertAdjacentHTML("afteren
    
 
   modalToggle();
+}
+
+function isSubstring(s1, s2){
+    var M = s1.length;
+    var N = s2.length;
+    for (var i = 0; i <= N - M; i++) {
+        var j;
+        for (j = 0; j < M; j++)
+            if (s2[i + j] != s1[j])
+                break;
+ 
+        if (j == M)
+            return i;
+    }
+ 
+    return -1;
 }
